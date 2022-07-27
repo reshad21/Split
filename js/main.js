13222b4985f0e50d4185e550bf4799835720a06d
reshad21 <miakala7@gmail.com>
2022-07-27 18:56:44 +0600

    js code for campaigns page

--- js/main.js
+++ js/main.js
@@ -1,20 +1,28 @@
 // let cmpdrpdwn = document.querySelector('#cmpdrpdwn');
 // let cmpdrpdwnitem = document.querySelector('#cmpdrpdwnitem');
 
-// cmpdrpdwn.addEventListener('click', function (e) {
-//     e.preventDefault();
-//     cmpdrpdwnitem.classList.toggle('active');
+// cmpdrpdwns.forEach(cmpdrpdwn => {
+//     cmpdrpdwn.addEventListener('click', function () {
+//         cmpdrpdwnitems.forEach(cmpdrpdwnitem => {
+//             cmpdrpdwnitem.classList.toggle('active');
+//         })
+//     })
 // })
 
+// ================campaigns page code============== //
+function campaigns(){
+    let cmpdrpdwns = document.querySelectorAll('#cmpdrpdwn');
+    let cmpdrpdwnitems = document.querySelectorAll('#cmpdrpdwnitem');
+    for (let i = 0; i < cmpdrpdwns.length; i++) {
+        const element = cmpdrpdwns[i];
+        element.addEventListener('click',function(){
+            cmpdrpdwnitems[i].classList.toggle('active');
+        });
+    }
+}
+campaigns();
 
 
-let cmpdrpdwns = document.querySelectorAll('#cmpdrpdwn');
-let cmpdrpdwnitems = document.querySelectorAll('#cmpdrpdwnitem');
 
-cmpdrpdwns.forEach(cmpdrpdwn => {
-    cmpdrpdwn.addEventListener('click', function () {
-        cmpdrpdwnitems.forEach(cmpdrpdwnitem => {
-            cmpdrpdwnitem.classList.toggle('active');
-        })
-    })
-})
+
+
