aa30589f29ea98e8776f0a30032a29e90f27c134
reshad21 <miakala7@gmail.com>
2022-07-29 18:19:10 +0600

    publlisher page start

--- js/main.js
+++ js/main.js
@@ -24,6 +24,22 @@ function campaigns(){
 }
 campaigns();
 
+// ===============Publisherdashbord page code============== //
+let toggleSection = document.querySelector('#toggle-section');
+let toggleIcon = document.querySelector('#toggle-icon');
+let logoSec = document.querySelector('.logo-sec');
+let clpsstore = document.querySelector('#clps-store');
+let mainstore = document.querySelector('#main-store');
+
+toggleIcon.addEventListener('click',function(e){
+    e.preventDefault();
+    toggleSection.classList.toggle('active');
+    logoSec.classList.toggle('active');
+    toggleIcon.classList.toggle('active');
+    clpsstore.classList.toggle('collaps');
+    mainstore.classList.toggle('active');
+})
+
 
 
 
